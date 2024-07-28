ee3d1818774377f636778e5dfde59e7918219a54
kamilaprz <kamila.przychodzen@storebrand.no>
2024-07-28 14:35:13 +0200

    Recurring events

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,18 +1,17 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import Card from 'react-bootstrap/Card';
-import {Button} from "react-bootstrap";
 
 import styles from './styles.module.css';
 import Link from "@docusaurus/core/lib/client/exports/Link";
 
 const BattleList = [
-    {
-        title: "Expedition facility Lvl 1",
-        battleDate: "27.07 - 23:45 UCT",
-        image: require('@site/static/img/facility.jpeg').default,
-        route: "/events/facility",
-    },
+    /*    {
+            title: "Expedition facility Lvl 1",
+            battleDate: "27.07 - 23:45 UCT",
+            image: require('@site/static/img/facility.jpeg').default,
+            route: "/events/facility",
+        },*/
     {
         title: "Foundry battle",
         battleDate: "28.07 - 19:00 UCT",
@@ -20,44 +19,53 @@ const BattleList = [
         route: "/events/foundry-battle",
     }]
 
-const EventsList = [{
-    title: 'Recurring events',
-    Svg: require('@site/static/img/calendar-svgrepo-com.svg').default,
-    description: (<div className="row">
-        <div className={clsx('col col--6')}>
-            <dl style={{textAlign: "left"}}>
-                <dt>28-29.07</dt>
-                <dd>Working overtime</dd>
-                <dt>28-31.07</dt>
-                <dd>Fortress battles</dd>
-                <dt>29.07-03.08</dt>
-                <dd>Alliance mobilisation</dd>
-                <dt>29.07 - 04.08</dt>
-                <dd>Alliance Championship</dd>
-                <dt>30-31.07</dt>
-                <dd>Develop new tech</dd>
-                <dt>30.07 - 01.08</dt>
-                <dd>Crazy Joe</dd>
-                <dt>31.07 - 02.08</dt>
-                <dd>Gina revenge</dd>
-            </dl>
-        </div>
-    </div>),
-}, /*  {
-        title: 'Tips and tricks',
-        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-          <>
-            Check here to find useful tips and tricks to improve your gameplay.
-          </>
-        ),
-      },*/];
+const EventsList = [
+    {
+        title: "Fortress battles",
+        startDate: "28.07.2024",
+        endDate: "31.07.2024",
+        route: "/events/fortress"
+    }, {
+        title: "Alliance mobilisation",
+        startDate: "29.07.2024",
+        endDate: "03.08.2024",
+        route: "/events/alliance-mobilisation"
+    }, {
+        title: "Working overtime",
+        startDate: "28.07.2024",
+        endDate: "29.07.2024",
+        route: "/events/working-overtime"
+    }, {
+        title: "Alliance Championship",
+        startDate: "29.07.2024",
+        endDate: "04.08.2024",
+        route: "/events/alliance-championship"
+    }, {
+        title: "Develop new tech",
+        startDate: "30.07.2024",
+        endDate: "31.07.2024",
+        route: "/events/dev-new-tech"
+    }, {
+        title: "Crazy Joe",
+        startDate: "31.07.2024",
+        endDate: "01.08.2024",
+        route: "/events/crazy-joe"
+    }, {
+        title: "Gina revenge",
+        startDate: "31.07.2024",
+        endDate: "02.08.2024",
+        route: "/events/gina-revenge"
+    }
+    , {
+        title: "Fishing tournament",
+        startDate: "30.07.2024",
+        endDate: "01.08.2024",
+        route: "/events/fishing-tournament"
+    }
+]
 
 function Feature({Svg, title, description}) {
     return (<div className={clsx('col col--12')}>
-        {/* <div className="text--center">
-            <Svg className={styles.featureSvg} role="img"/>
-        </div>*/}
         <div className="text--center padding-horiz--md">
             <Heading as="h3">{title}</Heading>
             <p>{description}</p>
@@ -67,7 +75,7 @@ function Feature({Svg, title, description}) {
 
 function BattleCard({title, battleDate, image, route}) {
     return (
-        <Link to={route} className="col col--4 margin-md">
+        <Link to={route} className="col col--4 margin-md shadow--md">
             <Card>
                 <Card.Img variant="top" src={image}/>
                 <Card.Body>
@@ -81,15 +89,39 @@ function BattleCard({title, battleDate, image, route}) {
     );
 }
 
+function RecurringEvent({startDate, endDate, title, route}) {
+    // Convert string dates to Date objects
+    const start = new Date(startDate.split('.').reverse().join('-')).setHours(0, 0, 0, 0);
+    const end = new Date(endDate.split('.').reverse().join('-')).setHours(23, 59, 59, 999);
+
+    // Get the current date
+    const current = new Date();
+
+    // Determine the className based on date comparison
+    const isOngoing = current >= start && current <= end;
+    return (
+        <li>
+            <Link to={route} className={" text--no-decoration "}>
+                <div className="text--primary">
+                    <span style={{display: isOngoing ? 'inline' : 'none'}} className="text--success"><b>Ongoing</b></span> {startDate} - {endDate} - {title}
+                </div>
+            </Link>
+        </li>
+    );
+}
+
 export default function HomepageFeatures() {
     return (<section className={styles.features}>
         <div className="container">
-            <div className="row">
+            <div className="row margin-top--md">
                 <Heading as="h2" className="text--center padding-horiz--md col col--12">Upcoming battles</Heading>
                 {BattleList.map((props, idx) => (<BattleCard key={idx} {...props} />))}
             </div>
-            <div className="row">
-                {EventsList.map((props, idx) => (<Feature key={idx} {...props} />))}
+            <div className="row margin-top--md">
+                <Heading as="h2" className="text--center padding-horiz--md col col--12">Recurring events</Heading>
+                <ul>
+                    {EventsList.map((props, idx) => (<RecurringEvent key={idx} {...props} />))}
+                </ul>
             </div>
         </div>
 
---
+++ src/pages/events/fortress.md
@@ -0,0 +1,7 @@
+---
+title: Fortress Battle
+description: Guide to the Fortress Battle
+hide_table_of_contents: false
+---
+
+To be done...
